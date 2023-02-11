5956460c1db8626a0d255ca2a238639266e47ae0
Edinaildo <edinaildopereira2013@gmail.com>
2023-02-10 21:53:09 -0300

    resolve o Module e cri a descrição do perfil

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Contact 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    contacts = Contact.objects.all()
+    return render(request, 'index.html', {'contacts': contacts})
 
 def addContact(request):
     if request.method == 'POST':
@@ -18,3 +19,7 @@ def addContact(request):
         
 
     return render(request, 'new.html')
+
+def contactProfile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'contact-profile.html', {'contact':contact})
